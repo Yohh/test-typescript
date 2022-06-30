5963da9f89878e2aa929a311d466e9944c0b2d43
Yohh <durandyohan@zaclys.net>
2022-06-30 21:50:00 +0200

    first test finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const testFunction = (arg: string): string => {
+  console.log(arg);
+  return arg;
+};
+
+const test: string = "test";
+const word: string = testFunction(test);
+const array: string[] = ["ceci", "est", "un", `${word}`];
+
+// const number: number = Math.random()*10
+// testFunction(number)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {array.map((elem, index) => (
+        <div key={index}>{elem}</div>
+      ))}
     </div>
   );
 }
